dabeaf7d984c14e453226a8526087eb8b59d261c
JoseNeto09 <joselopessobrinhoneto9@gmail.com>
2023-04-27 21:31:56 -0300

    add: deploy worlflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/myKanban/",
   test: {
     globals: true,
     environment: 'jsdom',
